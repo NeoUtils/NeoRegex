cc6d9c89ffbc333ddbdc4200dcfa5f2989c7fec4
Irineu A. Silva <irineu.silva@aiqfome.com>
2021-12-17 22:14:05 -0300

    feat : multi regex added

---
+++ app/src/main/java/com/neo/regex/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.neo.regex
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class MainViewModel : ViewModel() {
+    private val _expressions: MutableLiveData<MutableList<Expression>> by lazy {
+        MutableLiveData(mutableListOf(Expression(), Expression()))
+    }
+
+    val expressions: LiveData<MutableList<Expression>> get() = _expressions
+
+    fun addExpression() {
+        _expressions.value = _expressions.value?.apply {
+            add(Expression())
+        }
+    }
+
+    fun removeExpression(position: Int) {
+        _expressions.value = _expressions.value?.apply {
+            removeAt(position)
+        }
+    }
+
+}
