e150b851bb26d4b785bf034c55b338efca69cb44
Irineu333 <mmmirineusilva@gmail.com>
2024-08-27 23:30:46 -0300

    chore: history control ui

--- build-logic/src/main/kotlin/com.neo.regex.multiplatform.gradle.kts
+++ build-logic/src/main/kotlin/com.neo.regex.multiplatform.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.ui)
             implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.components.resources)
         }
 
