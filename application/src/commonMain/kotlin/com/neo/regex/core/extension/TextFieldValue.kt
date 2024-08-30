ce80bc59f153d6516fa42b6b7f1183a7f0446943
Irineu333 <mmmirineusilva@gmail.com>
2024-08-30 18:03:42 -0300

    fix: don't allow multiline text pasting

--- application/src/commonMain/kotlin/com/neo/regex/core/extension/TextFieldValue.kt
+++ application/src/commonMain/kotlin/com/neo/regex/core/extension/TextFieldValue.kt
@@ -3,7 +3,11 @@ package com.neo.regex.core.extension
 import androidx.compose.ui.text.input.TextFieldValue
 import com.neo.regex.core.domain.model.Input
 
-fun TextFieldValue.toInput() = Input(
-    text = text,
+fun TextFieldValue.toInput(allowMultiline: Boolean = true) = Input(
+    text =  if (allowMultiline) {
+        text
+    } else {
+        text.replace("\n", "")
+    },
     selection = selection
 )
