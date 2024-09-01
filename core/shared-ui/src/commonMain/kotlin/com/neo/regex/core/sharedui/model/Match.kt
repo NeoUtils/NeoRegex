0223d8d554ec1f812766fa6212967b5f7ea5bcbb
Irineu333 <mmmirineusilva@gmail.com>
2024-09-01 17:04:55 -0300

    feat(desktop, text-editor): improve match tooltip appearance

--- core/shared-ui/src/commonMain/kotlin/com/neo/regex/core/sharedui/model/Match.kt
+++ core/shared-ui/src/commonMain/kotlin/com/neo/regex/core/sharedui/model/Match.kt
@@ -4,32 +4,4 @@ data class Match(
     val text: String,
     val range: IntRange,
     val groups: List<String> = emptyList()
-) {
-    override fun toString(): String {
-
-        val range = "range: $range"
-
-        if (groups.isEmpty()) {
-            return range
-        }
-
-        val groups = groups.mapIndexed { index, group ->
-            "$index: $group"
-        }
-
-        val separator = "─".repeat(
-            listOf(
-                range,
-                *groups.toTypedArray()
-            ).maxBy {
-                it.length
-            }.length
-        )
-
-        return listOf(
-            range,
-            separator,
-            *groups.toTypedArray()
-        ).joinToString("\n")
-    }
-}
+)
