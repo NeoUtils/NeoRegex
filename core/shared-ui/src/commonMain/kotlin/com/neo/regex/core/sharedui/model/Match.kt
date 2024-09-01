a162ee3e18d4dde52db948f0e4970c9b57846fcd
Irineu333 <mmmirineusilva@gmail.com>
2024-09-01 02:42:28 -0300

    chore(desktop, text-editor): show tooltip for match under mouse

--- core/shared-ui/src/commonMain/kotlin/com/neo/regex/core/sharedui/model/Match.kt
+++ core/shared-ui/src/commonMain/kotlin/com/neo/regex/core/sharedui/model/Match.kt
@@ -3,4 +3,8 @@ package com.neo.regex.core.sharedui.model
 data class Match(
     val start: Int,
     val end: Int
-)
+) {
+    override fun toString(): String {
+        return "range: $start - ${end - 1}"
+    }
+}
