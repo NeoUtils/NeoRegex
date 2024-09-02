e6c6692eb8b12d18aceaad135fc0e884d740fdef
Irineu333 <mmmirineusilva@gmail.com>
2024-09-02 16:40:50 -0300

    chore(android, text-editor): open match in a bottom sheet

--- core/shared-ui/src/androidMain/kotlin/com/neo/regex/core/sharedui/TextEditor.android.kt
+++ core/shared-ui/src/androidMain/kotlin/com/neo/regex/core/sharedui/TextEditor.android.kt
@@ -1,6 +1,8 @@
 package com.neo.regex.core.sharedui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,25 +10,36 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
 import com.neo.regex.core.sharedui.extension.getBoundingBoxes
 import com.neo.regex.core.sharedui.model.Match
+import com.neo.regex.core.sharedui.model.MatchBox
 import com.neo.regex.designsystem.theme.Blue100
 import com.neo.regex.designsystem.theme.NeoTheme.dimensions
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 actual fun TextEditor(
     value: TextFieldValue,
@@ -44,6 +57,47 @@ actual fun TextEditor(
     val scrollState = rememberScrollState()
 
     var textLayout by remember { mutableStateOf<TextLayoutResult?>(null) }
+
+    var pressedMatch by remember { mutableStateOf<Match?>(null) }
+
+    val interactionSource = remember { MutableInteractionSource() }
+
+    var selectedMatch by remember { mutableStateOf<Match?>(null) }
+
+    val keyboard = LocalSoftwareKeyboardController.current
+
+    LaunchedEffect(interactionSource, matches) {
+        interactionSource.interactions.collect { interaction ->
+            when (interaction) {
+                is PressInteraction.Press -> {
+                    pressedMatch = textLayout?.let { textLayout ->
+                        matches.firstOrNull { match ->
+                            textLayout
+                                .getBoundingBoxes(
+                                    match.range.first,
+                                    match.range.last
+                                )
+                                .any {
+                                    it.contains(interaction.pressPosition)
+                                }
+                        }
+                    }
+                }
+
+                is PressInteraction.Cancel -> {
+                    pressedMatch = null
+                }
+
+                is PressInteraction.Release -> {
+                    if (pressedMatch != null) {
+                        keyboard?.hide()
+                    }
+                    selectedMatch = pressedMatch
+                    pressedMatch = null
+                }
+            }
+        }
+    }
 
     Row(modifier) {
 
@@ -75,9 +129,7 @@ actual fun TextEditor(
                     trim = LineHeightStyle.Trim.None
                 )
             ),
-            onTextLayout = {
-                textLayout = it
-            },
+            interactionSource = interactionSource,
             modifier = Modifier
                 .padding(start = dimensions.tiny)
                 .weight(weight = 1f, fill = false)
@@ -85,8 +137,8 @@ actual fun TextEditor(
                 .verticalScroll(scrollState) // TODO(improve): https://github.com/NeoUtils/NeoRegex/issues/15
                 .onFocusChanged(onFocusChange)
                 .drawBehind {
-                    textLayout?.let { textLayout ->
-                        val boxes = runCatching {
+                    val matchBoxes = textLayout?.let { textLayout ->
+                        runCatching {
                             matches.flatMap { match ->
                                 textLayout
                                     .getBoundingBoxes(
@@ -94,22 +146,62 @@ actual fun TextEditor(
                                         match.range.last
                                     )
                                     .map {
-                                        it.deflate(
-                                            delta = mergedTextStyle.letterSpacing.toPx()
+                                        MatchBox(
+                                            match,
+                                            it.deflate(
+                                                delta = 0.8f
+                                            )
                                         )
                                     }
                             }
-                        }.getOrElse { listOf() }
+                        }.getOrNull()
+                    } ?: listOf()
 
-                        boxes.forEach {
+                    matchBoxes.forEach { (_, rect) ->
+                        drawRect(
+                            color = Blue100,
+                            topLeft = Offset(rect.left, rect.top),
+                            size = Size(rect.width, rect.height)
+                        )
+                    }
+
+                    pressedMatch?.let { match ->
+                        val matchBox = matchBoxes.firstOrNull { (it, _) ->
+                            it == match
+                        }
+
+                        matchBox?.let { (_, rect) ->
                             drawRect(
-                                color = Blue100,
-                                topLeft = Offset(it.left, it.top),
-                                size = Size(it.width, it.height)
+                                color = Color.DarkGray,
+                                topLeft = Offset(rect.left, y = rect.top - scrollState.value),
+                                size = Size(rect.width, rect.height),
+                                style = Stroke(
+                                    width = 1f
+                                )
                             )
                         }
                     }
                 },
+            onTextLayout = {
+                textLayout = it
+            }
         )
+
+        selectedMatch?.let {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    selectedMatch = null
+                }
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        append("range: ${it.range}")
+                    },
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(16.dp)
+                )
+            }
+        }
     }
 }
---
+++ core/shared-ui/src/commonMain/kotlin/com/neo/regex/core/sharedui/model/MatchBox.kt
@@ -0,0 +1,8 @@
+package com.neo.regex.core.sharedui.model
+
+import androidx.compose.ui.geometry.Rect
+
+data class MatchBox(
+    val match: Match,
+    val rect: Rect
+)
