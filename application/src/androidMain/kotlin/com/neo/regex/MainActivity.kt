f35c9deadcb7eb4264bf20ec69f25ebdea6e1945
Irineu333 <mmmirineusilva@gmail.com>
2024-09-15 00:25:36 -0300

    feat(android): configure system bars

--- application/src/androidMain/kotlin/com/neo/regex/MainActivity.kt
+++ application/src/androidMain/kotlin/com/neo/regex/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.neo.regex
 
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.neo.regex.core.designsystem.theme.NeoTheme
 import com.neo.regex.ui.App
@@ -13,11 +20,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupSystemBars()
+
         setContent {
             NeoTheme {
-                App()
+                App(Modifier.safeDrawingPadding())
             }
         }
+    }
+
+    private fun setupSystemBars() {
+        val style = when (resources.configuration.uiMode and UI_MODE_NIGHT_MASK) {
+            UI_MODE_NIGHT_YES -> {
+                SystemBarStyle.dark(
+                    Color.BLACK,
+                )
+            }
+
+            else -> {
+                SystemBarStyle.light(
+                    Color.WHITE,
+                    Color.BLACK,
+                )
+            }
+        }
+
+        enableEdgeToEdge(style, style)
     }
 }
 
--- application/src/commonMain/kotlin/com/neo/regex/ui/App.kt
+++ application/src/commonMain/kotlin/com/neo/regex/ui/App.kt
@@ -1,13 +1,16 @@
 package com.neo.regex.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.FadeTransition
 import com.neo.regex.core.designsystem.theme.NeoBackground
 import com.neo.regex.feature.matcher.MatcherScreen
 
 @Composable
-fun App() = NeoBackground {
+fun App(
+    modifier: Modifier = Modifier
+) = NeoBackground(modifier) {
     Navigator(MatcherScreen()) { navigator ->
         FadeTransition(navigator) {
             it.Content()
