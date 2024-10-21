57c9cab9f0b401d7a21e41833a7e4bb6009919d4
Irineu A. Silva <mmmirineusilva@gmail.com>
2024-10-20 23:24:03 -0300

    fix(desktop): improve window state detection

---
+++ core/shared-ui/src/desktopMain/kotlin/com/neoutils/neoregex/core/sharedui/extension/ComposeWindow.kt
@@ -0,0 +1,36 @@
+/*
+ * NeoRegex.
+ *
+ * Copyright (C) 2024 Irineu A. Silva.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.neoutils.neoregex.core.sharedui.extension
+
+import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.window.WindowPlacement
+import java.awt.Frame.MAXIMIZED_HORIZ
+
+val ComposeWindow.isFloating
+    get() = placement == WindowPlacement.Floating
+
+val ComposeWindow.isFullMaximized
+    get() = placement == WindowPlacement.Maximized &&
+            extendedState and MAXIMIZED_HORIZ != 0
+
+val ComposeWindow.isMaximized
+    get() = placement == WindowPlacement.Maximized
+
+val ComposeWindow.isFullscreen
+    get() = placement == WindowPlacement.Fullscreen
