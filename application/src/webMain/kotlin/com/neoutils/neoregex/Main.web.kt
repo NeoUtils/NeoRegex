d397eed59010606b2343c417433f839d66a68786
Irineu A. Silva <mmmirineusilva@gmail.com>
2024-11-28 22:49:36 -0300

    feat(web, text-editor): add experimental label

--- application/src/webMain/kotlin/com/neoutils/neoregex/Main.web.kt
+++ application/src/webMain/kotlin/com/neoutils/neoregex/Main.web.kt
@@ -18,8 +18,23 @@
 
 package com.neoutils.neoregex
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.CanvasBasedWindow
 import com.neoutils.neoregex.core.common.extension.toCss
 import com.neoutils.neoregex.core.common.util.SizeManager
@@ -49,8 +64,42 @@ fun main() {
                 body.style.backgroundColor =
                     colorScheme.background.toCss()
 
-                App()
+                Experimental {
+                    App()
+                }
             }
         }
+    }
+}
+
+@Composable
+fun Experimental(
+    content: @Composable () -> Unit
+) = Box {
+    content()
+
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .align(Alignment.TopEnd)
+    ) {
+        Text(
+            text = "experimental",
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+                .graphicsLayer(
+                    rotationZ = 45f,
+                    translationX = 50f,
+                    translationY = -50f
+                )
+                .background(Color.Yellow)
+                .padding(
+                    vertical = 8.dp
+                ),
+        )
     }
 }
