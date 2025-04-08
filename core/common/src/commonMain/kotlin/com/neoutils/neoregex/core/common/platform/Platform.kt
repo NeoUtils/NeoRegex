b3b9a79b38cad63b50433e7f1e3cdbde3448c2f9
Irineu A. Silva <mmmirineusilva@gmail.com>
2025-04-08 00:53:14 -0300

    chore(common): small improvements

--- core/common/src/commonMain/kotlin/com/neoutils/neoregex/core/common/platform/Platform.kt
+++ core/common/src/commonMain/kotlin/com/neoutils/neoregex/core/common/platform/Platform.kt
@@ -32,4 +32,6 @@ sealed class Platform {
     data object Web : Platform()
 }
 
+val Platform.isAndroid get() = this == Platform.Android
+
 expect val platform: Platform
--- core/shared-ui/src/commonMain/kotlin/com/neoutils/neoregex/core/sharedui/component/Footer.kt
+++ core/shared-ui/src/commonMain/kotlin/com/neoutils/neoregex/core/sharedui/component/Footer.kt
@@ -18,15 +18,19 @@
 
 package com.neoutils.neoregex.core.sharedui.component
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
@@ -35,19 +39,26 @@ import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.neoutils.highlight.compose.remember.rememberTextFieldValue
 import com.neoutils.neoregex.core.common.extension.toText
-import com.neoutils.neoregex.core.common.model.HistoryState
+import com.neoutils.neoregex.core.common.extension.toTextFieldValue
 import com.neoutils.neoregex.core.common.model.Field
+import com.neoutils.neoregex.core.common.model.HistoryState
 import com.neoutils.neoregex.core.common.model.TextState
+import com.neoutils.neoregex.core.common.platform.isAndroid
+import com.neoutils.neoregex.core.common.platform.platform
 import com.neoutils.neoregex.core.common.util.Command
 import com.neoutils.neoregex.core.common.util.Syntax
 import com.neoutils.neoregex.core.designsystem.textfield.NeoTextField
 import com.neoutils.neoregex.core.designsystem.theme.NeoTheme.dimensions
+import com.neoutils.neoregex.core.manager.salvage.SalvageManager
 import com.neoutils.neoregex.core.resources.Res
 import com.neoutils.neoregex.core.resources.matcher_footer_insert_regex_hint
-import com.neoutils.neoregex.core.common.extension.toTextFieldValue
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.koinInject
 
 sealed class FooterAction {
     data class UpdateRegex(
@@ -73,6 +84,7 @@ fun Footer(
     pattern: TextState,
     history: HistoryState,
     modifier: Modifier = Modifier,
+    salvageManager: SalvageManager = koinInject(),
     onFocus: (FocusState) -> Unit = {},
     onAction: (FooterAction) -> Unit = {},
     tooling: (@Composable () -> Unit)? = null,
@@ -84,82 +96,144 @@ fun Footer(
     color = colorScheme.surfaceContainer,
     contentColor = colorScheme.onSurface,
 ) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
 
-        val textFieldValue = remember(pattern) { pattern.toTextFieldValue() }
+    val coroutine = rememberCoroutineScope()
 
-        NeoTextField(
-            value = syntax
-                .highlight
-                .rememberTextFieldValue(textFieldValue),
-            onValueChange = {
-                onAction(
-                    FooterAction.UpdateRegex(
-                        it.toText()
-                    )
-                )
-            },
-            modifier = Modifier
-                .weight(weight = 1f)
-                .onFocusChanged(onFocus)
-                .onPreviewKeyEvent {
-                    when (Command.from(it)) {
-                        Command.UNDO -> {
-                            onAction(
-                                FooterAction.History.Undo(Field.REGEX)
-                            )
-                            true
-                        }
+    Column {
+        if (platform.isAndroid) {
+            AnimatedContent(
+                modifier = Modifier.fillMaxWidth(),
+                targetState = salvageManager
+                    .flow
+                    .collectAsStateWithLifecycle(
+                        initialValue = null
+                    ).value,
+                contentKey = { it != null },
+                transitionSpec = {
+                    val showUp = fadeIn() + slideIntoContainer(SlideDirection.Up)
+                    val hideDown = fadeOut() + slideOutOfContainer(SlideDirection.Down)
 
-                        Command.REDO -> {
-                            onAction(
-                                FooterAction.History.Redo(Field.REGEX)
-                            )
-                            true
-                        }
-
-                        else -> false
-                    }
+                    showUp togetherWith hideDown
                 },
-            singleLine = true,
-            hint = {
-                Text(
-                    text = stringResource(Res.string.matcher_footer_insert_regex_hint),
-                    style = LocalTextStyle.current.let {
-                        it.copy(
-                            color = it.color.copy(
-                                alpha = 0.5f
-                            )
-                        )
-                    },
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
-            },
-        )
+                contentAlignment = Alignment.Center
+            ) { opened ->
+                if (opened != null) {
+                    SalvageUi(
+                        modifier = Modifier.height(30.dp),
+                        opened = opened,
+                        onAction = { action ->
+                            when (action) {
+                                SalvageAction.Close -> {
+                                    coroutine.launch {
+                                        salvageManager.close()
+                                    }
+                                }
 
-        tooling?.let {
-            Row(
-                modifier = Modifier
-                    .padding(vertical = dimensions.small)
-                    .padding(end = dimensions.default)
-            ) {
-                it()
+                                SalvageAction.Update -> {
+                                    coroutine.launch {
+                                        salvageManager.update()
+                                    }
+                                }
+
+                                is SalvageAction.ChangeName -> {
+                                    coroutine.launch {
+                                        salvageManager.update {
+                                            it.copy(
+                                                title = action.name
+                                            )
+                                        }
+                                    }
+                                }
+
+                                SalvageAction.Reset -> {
+                                    coroutine.launch {
+                                        salvageManager.sync()
+                                    }
+                                }
+                            }
+                        }
+                    )
+                }
             }
         }
 
-        History(
-            history = history,
-            onAction = onAction,
-            modifier = Modifier
-                .padding(vertical = dimensions.small)
-                .padding(end = dimensions.small)
-                .focusProperties {
-                    canFocus = false
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            val textFieldValue = remember(pattern) { pattern.toTextFieldValue() }
+
+            NeoTextField(
+                value = syntax
+                    .highlight
+                    .rememberTextFieldValue(textFieldValue),
+                onValueChange = {
+                    onAction(
+                        FooterAction.UpdateRegex(
+                            it.toText()
+                        )
+                    )
+                },
+                modifier = Modifier
+                    .weight(weight = 1f)
+                    .onFocusChanged(onFocus)
+                    .onPreviewKeyEvent {
+                        when (Command.from(it)) {
+                            Command.UNDO -> {
+                                onAction(
+                                    FooterAction.History.Undo(Field.REGEX)
+                                )
+                                true
+                            }
+
+                            Command.REDO -> {
+                                onAction(
+                                    FooterAction.History.Redo(Field.REGEX)
+                                )
+                                true
+                            }
+
+                            else -> false
+                        }
+                    },
+                singleLine = true,
+                hint = {
+                    Text(
+                        text = stringResource(Res.string.matcher_footer_insert_regex_hint),
+                        style = LocalTextStyle.current.let {
+                            it.copy(
+                                color = it.color.copy(
+                                    alpha = 0.5f
+                                )
+                            )
+                        },
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1
+                    )
+                },
+            )
+
+            tooling?.let {
+                Row(
+                    modifier = Modifier
+                        .padding(vertical = dimensions.small)
+                        .padding(end = dimensions.default)
+                ) {
+                    it()
                 }
-        )
+            }
+
+            History(
+                history = history,
+                onAction = onAction,
+                modifier = Modifier
+                    .padding(vertical = dimensions.small)
+                    .padding(end = dimensions.small)
+                    .focusProperties {
+                        canFocus = false
+                    }
+            )
+        }
     }
 }
