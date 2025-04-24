317a23155174ab706c53a0a17c6f67c6c236ced6
Irineu A. Silva <mmmirineusilva@gmail.com>
2025-04-24 09:27:36 -0300

    chore(version): develop v3.2.0

--- build-logic/src/main/kotlin/extension/Project.kt
+++ build-logic/src/main/kotlin/extension/Project.kt
@@ -26,9 +26,9 @@ import org.gradle.kotlin.dsl.the
 val config = Config(
     version = Config.Version(
         major = 3,
-        minor = 1,
+        minor = 2,
         patch = 0,
-        phase = Config.Phase.RELEASE
+        phase = Config.Phase.DEVELOP
     ),
     android = Config.Android(
         compileSdk = 34,
